e15714d105f60fc47ae817ed2e5882d586b80b71
Ghassan Sohail <Ghassansohail@gmail.com>
2021-11-18 21:11:43 +0300

    Creates app

---
+++ app/src/main/java/com/ghassan/practicelecture10onwards/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ghassan.practicelecture10onwards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
